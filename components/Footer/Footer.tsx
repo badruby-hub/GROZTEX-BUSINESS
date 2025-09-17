1130ab18263b2a3e701206cd73753b7f2bb9854d
Nazim Fataliev <nazim.rabota@bk.ru>
2025-09-17 16:17:40 +0300

    +

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -13,13 +13,13 @@ export default function Footer() {
             <div className={classes.block__btn}><Link className={`${classes.link__btn} ${classes.btn__for__exchange__two}`} href="https://t.me/GROZTEX_bot">Обменять USDT</Link></div>
            </article>
            <article className={classes.block__info__two}>
-            <h4>info</h4>
+            <h3>info</h3>
             <p className={classes.btn}><Link className={`${classes.link}`} href="#">Пользовательское соглашение</Link></p>
             <p className={classes.btn}><Link className={`${classes.link}`} href="#">Политика конфиденциальности</Link></p>
             <p className={classes.btn}><Link className={`${classes.link}`} href="#">Политика по борьбе с отмыванием денег</Link></p>
            </article>
              <article className={classes.block__info__three}>
-            <h4>Contact</h4>
+            <h3>Contact</h3>
             <p className={`${classes.btn} ${classes.chat__bot}`}><Link className={`${classes.link} ${classes.btn__groztex__bot}`} href="https://t.me/GROZTEX_bot">@GROZTEX_bot</Link></p>
             <p className={`${classes.btn} ${classes.chat__support}`}><Link className={`${classes.link} ${classes.btn__groztex__support}`} href="https://t.me/GROZTEX">@GROZTEX_Support</Link></p>
             <p className={`${classes.btn} ${classes.chat__email}`}><Link className={`${classes.link} ${classes.btn__groztex__info}`} href="#">info.groztex.com</Link></p>
