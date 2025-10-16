965e57e54263048f9c8875efbe328548e9266590
Nazim Fataliev <nazim.rabota@bk.ru>
2025-10-16 13:42:32 +0300

    добавил рабочую почту

--- components/Burger-Menu/Burger-list.tsx
+++ components/Burger-Menu/Burger-list.tsx
@@ -19,12 +19,12 @@ export default function MenuList({active, setActive}:{active:boolean; setActive:
       </ul>
       <div>
       <div className={classes.block__btn}><Link className={`${classes.link} ${classes.btn__for__exchange}`} href="https://t.me/GROZTEX_bot">Обменять</Link></div>
-      <div className={classes.block__btn}><Link className={`${classes.link} ${classes.btn__for__news}`} href="https://t.me/groztex_news">подписаться</Link></div>
+      <div className={classes.block__btn}><Link className={`${classes.link} ${classes.btn__for__news}`} href="https://t.me/groztex_news">Подписаться</Link></div>
       </div>
      <article className={classes.block__info__three}>
             <p className={`${classes.btn} ${classes.chat__bot}`}><Link className={`${classes.link} ${classes.btn__groztex__bot}`} href="https://t.me/GROZTEX_bot">@GROZTEX_bot</Link></p>
             <p className={`${classes.btn} ${classes.chat__news}`}><Link className={`${classes.link} ${classes.btn__groztex__news}`} href="https://t.me/groztex_news">Новостной канал</Link></p>
-            <p className={`${classes.btn} ${classes.chat__email}`}><Link className={`${classes.link} ${classes.btn__groztex__info}`} href="#">info.groztex.com</Link></p>
+            <p className={`${classes.btn} ${classes.chat__email}`}><Link className={`${classes.link} ${classes.btn__groztex__info}`} href="mailto:groztex@yandex.ru?subject=Запрос%20с%20сайта&body=Здравствуйте">groztex@yandex.ru</Link></p>
             <p className={`${classes.btn} ${classes.chat__support}`}><Link className={`${classes.link} ${classes.btn__groztex__support}`} href="https://t.me/GROZTEX">@GROZTEX_Support</Link></p>
            </article>
            </section>
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -20,10 +20,10 @@ export default function Footer() {
            </article>
              <article className={classes.block__info__three}>
             <h3>Contact</h3>
+            <p className={`${classes.btn} ${classes.chat__news}`}><Link className={`${classes.link} ${classes.btn__groztex__news}`} href="https://t.me/groztex_news">Новостной канал</Link></p>
             <p className={`${classes.btn} ${classes.chat__bot}`}><Link className={`${classes.link} ${classes.btn__groztex__bot}`} href="https://t.me/GROZTEX_bot">@GROZTEX_bot</Link></p>
             <p className={`${classes.btn} ${classes.chat__support}`}><Link className={`${classes.link} ${classes.btn__groztex__support}`} href="https://t.me/GROZTEX">@GROZTEX_Support</Link></p>
-            <p className={`${classes.btn} ${classes.chat__email}`}><Link className={`${classes.link} ${classes.btn__groztex__info}`} href="#">info.groztex.com</Link></p>
-            <p className={`${classes.btn} ${classes.chat__news}`}><Link className={`${classes.link} ${classes.btn__groztex__info}`} href="https://t.me/groztex_news">Новостной канал</Link></p>
+            <p className={`${classes.btn} ${classes.chat__email}`}><Link className={`${classes.link} ${classes.btn__groztex__info}`} href="mailto:groztex@yandex.ru?subject=Запрос%20с%20сайта&body=Здравствуйте">groztex@yandex.ru</Link></p>
            </article>
       </section>
           </footer>
